a61758374e14d7e96273df46969528e382a40e59
Kyle Mellander <kylemellander@gmail.com>
2015-09-25 12:48:46 -0700

    Refactor out unnecessary code

--- app/routes/new-question.js
+++ app/routes/new-question.js
@@ -1,9 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return this.store.findAll('question');
-  },
   actions: {
     saveQuestion(params) {
       var newQuestion = this.store.createRecord('question', params);
